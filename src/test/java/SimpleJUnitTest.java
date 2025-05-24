f78acba46d725265c286c633849c254bc85212ed
Oganesyan Karen <kar99-99@bk.ru>
2025-05-24 12:51:52 +0300

    added JUnit 5

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.*;
+
+public class SimpleJUnitTest {
+
+    @BeforeAll
+        static void beforeAll() {
+        System.out.println("\nBefore All\n");
+    }
+
+    int result;
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each\n");
+        result = getResult();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("\nAfter All\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("First Test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        int resultt = getResult();
+        System.out.println("Second Test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        int resultt = getResult();
+        System.out.println("Third Test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
+    }
+}
